bbd099871401dc5acd476c2fe3ef32f7ab6a0f38
easyManik <easymanik151199@gmail.com>
2023-03-20 12:01:14 +0700

    commit

--- src/SimpleQuicks/Quicks.jsx
+++ src/SimpleQuicks/Quicks.jsx
@@ -5,7 +5,7 @@ import tasks from "./../images/tasks.png";
 import Search from "../component/search";
 import axios from "axios";
 import avatar from "./../images/Group 1603.png";
-import { useParams, useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import HeaderTasks from "../component/headerTask";
 import Moment from "react-moment";
 import schedule from "./../images/schedule_24px.png";
